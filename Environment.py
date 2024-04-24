83848f7c25c2c81cf137fb5f3e101396fff0f332
Ëzo-Tanuki <michailbryanlie@gmail.com>
2024-04-24 16:43:51 +0700

    first commit

---
+++ Environment.py
@@ -0,0 +1,7 @@
+class Environment():
+    def __init__(self, weight:int, height:int):
+        self.weight = weight
+        self.height = height
+
+        self.dynamicObject = list()
+        self.staticObject = list()
